438e91281ff4c1adbe69e29f1f80aea37d62bf70
David <david.espinosa.21@gmail.com>
2020-07-07 00:16:18 -0500

    Improved runtime
    Improved runtime from 3 ms (faster than 5.05%) to 1 ms (faster than 58.54%)

--- reverse_string.java
+++ reverse_string.java
@@ -1,16 +1,16 @@
 class Solution {
     public void reverseString(char[] s) {
-        // 3 ms, faster than 5.05%
-        // 53.4 mb, less than 8.31%
+        // 1 ms, faster than 58.54%
+        // 53.4 mb, less than 9.23%
+        
+        char temp = 0;
         
         for(int i = 0; i < s.length / 2; i++)
-            swap(s, i, s.length - 1 - i);
-    }
-    
-    public void swap(char[] s, int a, int b)
-    {
-        char temp = s[a];
-        s[a] = s[b];
-        s[b] = temp;
+        {
+            temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+        
     }
 }
