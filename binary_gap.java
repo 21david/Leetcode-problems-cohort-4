72a10a63f9335e163aa5b82d35b67366fc46d071
David <david.espinosa.21@gmail.com>
2020-07-07 03:02:29 -0500

    Improved runtime
    Improved runtime from 4 ms (faster than 6.29%) to 1 ms (faster than 54.45%).
    Runtime varies across execution, so these are approximate numbers.

--- binary_gap.java
+++ binary_gap.java
@@ -1,23 +1,23 @@
 class Solution {
     public int binaryGap(int N) {
-        // 4 ms, faster than 6.29%
-        // 37.9 mb, less than 17.45%
+        // 1 ms, faster than 54.45%
+        // 36.1 mb, less than 89.26%
         
         // convert to binary string so we can work with it
-        String binary = Integer.toBinaryString(N);
+        StringBuilder binary = new StringBuilder(Integer.toBinaryString(N));
         
         // we need to trim outside 0s (from the left and from the right)
-        binary = binary.substring(binary.indexOf("1"), binary.lastIndexOf("1") + 1);
+        binary = new StringBuilder(binary.substring(binary.indexOf("1"), binary.lastIndexOf("1") + 1));
 
         // if there is no pair of 1s (i.e. for 8), we return 0
         if(binary.length() == 1)
             return 0;
         
         // we need to find the longest string of 0s in our string
-        String zeros = ""; //new StringBuilder();
+        StringBuilder zeros = new StringBuilder(); //new StringBuilder();
         
-        while( binary.contains(zeros) )
-            zeros += "0";
+        while( binary.indexOf(zeros.toString()) >= 0 )
+            zeros.append("0");
         
         
         // the length of this string of 0s is the answer
