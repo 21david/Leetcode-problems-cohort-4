dbacce096861e5e4d825935b9ceff4210530d643
David <david.espinosa.21@gmail.com>
2020-07-14 16:23:19 -0500

    Added alternate solutions
    I added two solutions that use a multiset approach. One of them used a HashMap<Character, Integer> to store the multiset, the other one used an int[] array. I found that the one that used the int[] array ran much faster (2 ms as opposed to 9 ms) despite being the same approach.

--- find_the_difference.java
+++ find_the_difference.java
@@ -17,4 +17,63 @@ class Solution {
         
         return (char) letter;
     }
+    
+    public char findTheDifferenceMultiset(String s, String t)
+    {
+        // 9 ms, faster than 14.02%
+        // 38.4 MB, less than 23.97%
+        
+        // 1. make a multiset that counts the total frquency of every letter in s
+        // 2. Iterate through letters in t, subtracting from the frequency of each letter in the multiset
+        //     a. When a letter is reached that has a count of 0 in the multiset, this is the added letter
+        //     b. or, if a letter is reached that isn't already in the multiset, this is the added letter
+        
+        
+        // 1. 
+        HashMap<Character, Integer> multiset = new HashMap<Character, Integer>();
+        
+        for(char c : s.toCharArray())
+            multiset.put(c, multiset.getOrDefault(c, 0) + 1);
+        
+        // 2.
+        for(char c : t.toCharArray())
+            if(multiset.containsKey(c))  // a.
+            {
+                if(multiset.get(c) == 0)
+                    return c;
+                else
+                    multiset.put(c, multiset.get(c) - 1);
+            }
+            else
+                return c;  // b.
+        
+        return 0;
+    }
+    
+    public char findTheDifferenceMultiset2(String s, String t)
+    {
+        // 2 ms, faster than 60.46%
+        // 37.9 MB, less than 53.17%
+        
+        // 1. make a multiset that counts the total frquency of every letter in s
+        // 2. Iterate through letters in t, subtracting from the frequency of each letter in the multiset
+        //     a. When a letter is reached that has a count of 0 in the multiset, this is the added letter
+        
+        
+        // 1. 
+        int[] multiset = new int[26];
+        
+        for(char c : s.toCharArray())
+            multiset[c - 'a']++;
+        
+        // 2.
+        for(char c : t.toCharArray())
+            if(multiset[c - 'a'] >= 1)  
+                multiset[c - 'a']--;
+            else  // a.
+                return c;  
+        
+        return 0;
+    }
+    
 }
