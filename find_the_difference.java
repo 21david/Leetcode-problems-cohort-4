9bc4f79c5fc9a75568735c7e5289efd65f7e9bc5
David <david.espinosa.21@gmail.com>
2020-07-14 16:04:45 -0500

    Improved space complexity
    Improved space complexity by eliminating a variable (String st). Space complexity was O(S + T), now is O(1).

--- find_the_difference.java
+++ find_the_difference.java
@@ -1,17 +1,18 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         // 2 ms, faster than 60.46%
-        // 39.7 mb, less than 5.4%
+        // 39.3 mb, less than 16.23%
         
         // approach to 136. Single Number can be used here
         // Time complexity: O(S + T)
-        // Space complexity: O(S + T)
-        
-        String st = s + t;
+        // Space complexity: O(1)
         
         int letter = 0;
         
-        for(char c : st.toCharArray())
+        for(char c : s.toCharArray())
+            letter ^= c;
+        
+        for(char c : t.toCharArray())
             letter ^= c;
         
         return (char) letter;
