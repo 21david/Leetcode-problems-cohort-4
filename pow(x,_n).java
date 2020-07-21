c6fae2d613840b717619fa2237c0ccad747a5523
David <david.espinosa.21@gmail.com>
2020-07-21 18:50:16 -0500

    Added dynamic programming solution

--- pow(x,_n).java
+++ pow(x,_n).java
@@ -47,3 +47,59 @@ class Solution {
         }
     }
 }
+
+
+// dynamic programming solution (bottom up)
+class Solution {
+    public double myPow(double x, int nInput) {
+        // 1221 ms, faster than 5.09%
+        // 37 MB, less than 25.26%
+        
+        long n = nInput;
+        
+        if(n == 0)
+            return 1;
+        else if(n == Integer.MIN_VALUE)
+        {
+            if(x == 1)
+                return 1;
+            else if(x == -1)
+                return 1;
+            else
+                return 0;
+        }
+        else if(n == Integer.MAX_VALUE)
+        {
+            if(x == 1)
+                return 1;
+            else if(x == -1)
+                return -1;
+        }
+        
+        boolean neg = false;
+        
+        if(n < 0)
+        {
+            neg = true;
+            n = -n;
+        }
+        
+        long count = 1;
+        double p = x;
+        
+        while(count * 2 <= n)
+        {
+            p = p * p;
+            count *= 2;
+        }
+        
+        while(count++ < n)
+            p *= x;
+        
+        if(neg)
+            return 1 / p;
+        else
+            return p;
+    }
+    
+}
