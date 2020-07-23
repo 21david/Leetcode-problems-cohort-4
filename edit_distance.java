a512f21d4510155d4af9201f0ccb8d36abc946c2
David <david.espinosa.21@gmail.com>
2020-07-22 21:13:29 -0500

    Added dynamic programming approach

--- edit_distance.java
+++ edit_distance.java
@@ -50,6 +50,51 @@ class Solution {
 }
 
 
+// dynamic programming approach, bottom up
+class Solution {
+    public int minDistance(String word1, String word2) {
+        // 31 ms, faster than 5.37%
+        // 43.7 MB, less than 5.01%
+        
+        if(word1.length() == 0)
+            return word2.length();
+        else if(word2.length() == 0)
+            return word1.length();
+        
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        
+        for(int i = 0; i <= word1.length(); i++)
+            dp[i][0] = i;
+        
+        for(int i = 0; i <= word2.length(); i++)
+            dp[0][i] = i;
+        
+        for(int r = 1; r <= word1.length(); r++)
+        {
+            for(int c = 1; c <= word2.length(); c++)
+            {
+                if(word1.charAt(r-1) == word2.charAt(c-1)) // if they are equal
+                    dp[r][c] = dp[r-1][c-1];
+                else
+                    dp[r][c] = min(dp[r-1][c-1], dp[r-1][c], dp[r][c-1]) + 1;
+            }
+        }
+        System.out.println(Arrays.deepToString(dp).replace("],", "\n"));
+        
+        return dp[word1.length()][word2.length()];
+    }
+    
+    public static int min(int... args)
+    {
+        int min = args[0];
+        
+        for(int i = 1; i < args.length; i++)
+            if(min > args[i])
+                min = args[i];
+        
+        return min;
+    }
+}
 
 
 /*
